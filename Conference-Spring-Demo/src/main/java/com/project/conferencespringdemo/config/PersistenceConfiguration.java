7e7d2901b86607b9c8fb1c86b304fff2410586d6
Ishita Das <ishita.das9038@gmail.com>
2024-03-12 00:22:20 +0530

    Updated 12-March

--- Conference-Spring-Demo/src/main/java/com/project/conferencespringdemo/config/PersistenceConfiguration.java
+++ Conference-Spring-Demo/src/main/java/com/project/conferencespringdemo/config/PersistenceConfiguration.java
@@ -6,19 +6,20 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
+//Not in use, this is one type of DB setting can be used
 
-@Configuration
+//@Configuration
 public class PersistenceConfiguration {
 
     // Spring looks for the returned DataSource object and tries to see if one already exists in Spring context. If does, it will replace its definition with the one that is found
-    @Bean
-    public DataSource dataSource(){
-        // Body of the method is where configuration happens in java config class
-        DataSourceBuilder<?> builder = DataSourceBuilder.create();
-        builder.url("jdbc:mysql://localhost:3306/conference");
-        builder.username("root");
-        builder.password("MySQL@123");
-        System.out.println("Custom dataSource Bean has been initiated");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        // Body of the method is where configuration happens in java config class
+//        DataSourceBuilder<?> builder = DataSourceBuilder.create();
+//        builder.url("<DB_URL>");
+//        builder.username("<DB_Username>");
+//        builder.password("<DB_PASSWORD>");
+//        System.out.println("Custom dataSource Bean has been initiated");
+//        return builder.build();
+//    }
 }
